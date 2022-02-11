fb38ecd19282ef0adf40525971542dccc9c5a0b9
ERROR15515 <90280871+ERROR15515@users.noreply.github.com>
2022-02-11 17:41:49 +0300

    Update PZ_12_1.py

--- PZ_12/PZ_12_1.py
+++ PZ_12/PZ_12_1.py
@@ -1,14 +1,15 @@
 from tkinter import *
 
 
-def window_deleted():
-    print('Окно закрыто')
-    root.quit()
-
-
-root = Tk()
-root.title('Пример приложения')
+root = Tk()     # настройки окна
+root.title('Sign Up')
 root.geometry('600x800')
-root.protocol('WM_DELETE_WINDOW', window_deleted)
-root.resizable(True, False)
+root.resizable(False, False)
+
+# настройка кнопок
+button_can = Button(root, text='cancel', width=5, height=1, bg='red', fg='white', font='arial 10')
+button_can.place(x=530, y=760)
+button_sub = Button(root, text='submit', width=5, height=1, bg='green', fg='white', font='arial 10')
+button_sub.place(x=470, y=760)
+
 root.mainloop()
