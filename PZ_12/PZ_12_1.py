2acdd923364944d64ab0bf9d0fe08a54631c523e
ERROR15515 <90280871+ERROR15515@users.noreply.github.com>
2022-02-11 18:01:46 +0300

    Update PZ_12_1.py

--- PZ_12/PZ_12_1.py
+++ PZ_12/PZ_12_1.py
@@ -6,6 +6,11 @@ root.title('Sign Up')
 root.geometry('600x800')
 root.resizable(False, False)
 
+frame1 = Frame(root, bg='orange')
+frame1.place(relx=0.005, rely=0.005, relwidth=0.99, relheigh=0.99)
+frame2 = Frame(root, bg='black')
+frame2.place(relx=0.005, rely=0.01, relwidth=0.99, relheigh=0.8)
+
 # настройка кнопок
 button_can = Button(root, text='cancel', width=5, height=1, bg='red', fg='white', font='arial 10')
 button_can.place(x=530, y=760)
